7e862ce42d8c5d586e1d6c165cc3fb24c8118ee1
Mateus Zucco <mateuszuccco@gmail.com>
2022-01-19 22:30:40 -0300

    update hr_saida sql

--- src/modules/home/scripts/CarDao.php
+++ src/modules/home/scripts/CarDao.php
@@ -8,14 +8,19 @@ parking($plate);
 function parking($plate)
 {
     $conexao = criaConexao();
-    $sql = "select * from cars where placa = '{$plate}'";
+    $sql = "select * from veiculos where placa = '{$plate}'";
 
     $stmt = $conexao->prepare($sql);
     try {
         $stmt->execute();
         $car = $stmt->fetch(PDO::FETCH_ASSOC);
         if ($car){
-            echo $car['modelo'];
+            date_default_timezone_set("America/Sao_Paulo");
+            $hour = date("d-m-Y H:i:s");
+            $carId = $car['id'];         
+            $sqlSaidaCarro = "update entrada_saida es inner join veiculos v on es.veiculo = v.id set hr_saida = '{$hour}' where es.veiculo = '{$carId}'";
+            $stmt2 = $conexao->prepare($sqlSaidaCarro);
+            $stmt2->execute();
         }else{
             echo 'faz meu pau de trampolin';
         }
