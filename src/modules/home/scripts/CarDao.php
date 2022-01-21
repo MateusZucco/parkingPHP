983ab6d8cbedca19ff35f9febd3dbb4fcb72b947
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-01-20 23:05:26 -0300

    FINALIZADOOOOOOOOO

--- src/modules/home/scripts/CarDao.php
+++ src/modules/home/scripts/CarDao.php
@@ -13,29 +13,41 @@ function parking($plate)
     $stmt = $conexao->prepare($sql);
     try {
         $stmt->execute();
-        $car = $stmt->fetch(PDO::FETCH_ASSOC);
-        isCarRegistered($car, $plate, $conexao);
+        $entry = $stmt->fetch(PDO::FETCH_ASSOC);
+        error_log($entry['veiculo']);
+        isCarRegistered($entry, $plate, $conexao);
     } catch (PDOException $e) {
         echo $e->getMessage();
     }
 
 }
 
-function isCarRegistered($car, $plate, $conexao){
-    if ($car){        
-        date_default_timezone_set("America/Sao_Paulo");
-        $date = date('y-m-d H:i:s');
-        $sqlSaidaCarro = "update entrada_saida es inner join veiculos v on es.veiculo = v.id set hr_saida = :date where es.veiculo = :carId";
+function isCarRegistered($entry, $plate, $conexao){
+    date_default_timezone_set("America/Sao_Paulo");
+    $date = date('y-m-d H:i:s');
+    if ($entry && !$entry['hr_saida']){        
+        $sqlSaidaCarro = "update entrada_saida es inner join veiculos v on es.veiculo = v.id set hr_saida = :date where es.id = :carId";
         $stmt = $conexao->prepare($sqlSaidaCarro);
         $stmt->bindParam(":date", $date);
-        $stmt->bindParam(":carId", $car['id']);
+        $stmt->bindParam(":carId", $entry['id']);
         try{
             $stmt->execute();
             header("Location:../view/home.php");
         }catch (PDOException $e) {
             echo $e->getMessage();
         }
-    }else{
+    }else if ($entry['hr_saida']) {
+        $sqlEntradaCarro = "insert into entrada_saida(veiculo, hr_entrada) values(:carId, :date)";
+        $stmt = $conexao->prepare($sqlEntradaCarro);
+        $stmt->bindParam(":date", $date);
+        $stmt->bindParam(":carId", $entry['veiculo']);
+        try{
+            $stmt->execute();
+            header("Location:../view/home.php");
+        }catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    } else {
         header("Location:../view/register.php?plate=".$plate);
     }
     
